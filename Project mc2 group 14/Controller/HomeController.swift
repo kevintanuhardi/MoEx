c430c207159e31efbc8ef595d3173fc3be400831
Kevin Tanuhardi <kevin.tanuhardi@gmail.com>
2021-06-04 10:30:34 +0700

    Initial commit | MVC folder

---
+++ Project mc2 group 14/Controller/HomeController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project mc2 group 14
+//
+//  Created by Kevin Tanuhardi on 04/06/21.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
