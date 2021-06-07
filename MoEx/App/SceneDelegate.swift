db2c5b35661bcc27ca9403bda0f3e7e6658b52ac
Randy Efan Jayaputra <randyrandyrej8@gmail.com>
2021-06-07 18:25:00 +0700

    initial project

---
+++ MoEx/App/SceneDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  SceneDelegate.swift
+//  Project mc2 group 14
+//
+//  Created by Kevin Tanuhardi on 04/06/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        let viewController = ModuleExerciseViewController() // Handle Develop Here
+        let navigationBar = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationBar
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+
+
+}
+
---
+++ MoEx/Module/Module Exercise/ModuleExerciseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ModuleExerciseViewController.swift
+//  Project mc2 group 14
+//
+//  Created by Randy Efan Jayaputra on 07/06/21.
+//
+
+import UIKit
+
+class ModuleExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
