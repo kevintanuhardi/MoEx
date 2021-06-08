c8bb948d783640ab2cd980c99915071be8eb5479
Ahmad Zaky W <azakyw@gmail.com>
2021-06-08 15:29:34 +0700

    adding module, exercise, benefit, howto model

---
+++ MoEx/Model/ModuleModel.swift
@@ -0,0 +1,36 @@
+//
+//  Module.swift
+//  MoEx
+//
+//  Created by Ahmad Zaky on 08/06/21.
+//
+
+import Foundation
+import UIKit
+
+struct Module {
+    let title: String
+    let thumb: UIImage
+    let module_desc: String
+    let exercise: [Exercise]
+    let time: Int?
+    let calories: Int?
+}
+
+struct Exercise {
+//    let gif: String
+    let thumb: UIImage
+    let benefits: [Benefit]
+    let equipment: String?
+    let reps: Int
+    let illustrations: [UIImage]
+    let howTo: [HowTo]
+}
+
+struct Benefit {
+    let description: String
+}
+
+struct HowTo {
+    let description: String
+}
