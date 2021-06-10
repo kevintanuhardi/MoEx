e16a405df56f3e93cece2b5b662a788821c300e0
Ahmad Zaky W <azakyw@gmail.com>
2021-06-10 11:06:12 +0700

    add PauseExercise

---
+++ MoEx/Module/PauseExercise/PauseExerciseViewController.swift
@@ -0,0 +1,37 @@
+//
+//  PauseExerciseViewController.swift
+//  MoEx
+//
+//  Created by Ahmad Zaky on 08/06/21.
+//
+
+import UIKit
+
+class PauseExerciseViewController: UIViewController {
+    
+    @IBOutlet weak var quitButton: UIButton!
+    @IBOutlet weak var resumeButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initComponent()
+        // Do any additional setup after loading the view.
+    }
+    
+    func initComponent() {
+        quitButton.makeCornerRadius(cornerRadius: 8)
+        resumeButton.makeCornerRadius(cornerRadius: 8)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
