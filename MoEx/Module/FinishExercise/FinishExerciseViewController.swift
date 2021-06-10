4b73aa9924204ed6229cbe76eb2157c03de3198a
Ahmad Zaky W <azakyw@gmail.com>
2021-06-10 11:05:43 +0700

    add FinishExercise

---
+++ MoEx/Module/FinishExercise/FinishExerciseViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FinishExerciseViewController.swift
+//  MoEx
+//
+//  Created by Ahmad Zaky on 08/06/21.
+//
+
+import UIKit
+
+class FinishExerciseViewController: UIViewController {
+    
+    
+    @IBOutlet weak var finishButton: UIButton!
+    @IBOutlet weak var moduleName: UILabel!
+    @IBOutlet weak var totalWorkout: UILabel!
+    @IBOutlet weak var workoutTime: UILabel!
+    @IBOutlet weak var circleLogoContainer: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initComponent()
+        // Do any additional setup after loading the view.
+    }
+    
+    func initComponent() {
+        finishButton.makeCornerRadius(cornerRadius: 8)
+        circleLogoContainer.layer.cornerRadius = circleLogoContainer.frame.height/2
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+}
