3aaf48277635881fab6894dc5e04465deff0c5e3
Ahmad Zaky W <azakyw@gmail.com>
2021-06-15 11:34:09 +0700

    add current exercise Index

--- MoEx/Module/FinishExercise/FinishExerciseViewController.swift
+++ MoEx/Module/FinishExercise/FinishExerciseViewController.swift
@@ -16,19 +16,32 @@ class FinishExerciseViewController: UIViewController {
     @IBOutlet weak var workoutTime: UILabel!
     @IBOutlet weak var circleLogoContainer: UIView!
 
+    var moduleModel: ModuleModel?
+    var workoutIndex: Int?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initComponent()
         // Do any additional setup after loading the view.
     }
+
     
     func initComponent() {
+        guard let currentWorkout = workoutIndex else {
+            return
+        }
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
         finishButton.makeCornerRadius(cornerRadius: 8)
         circleLogoContainer.layer.cornerRadius = circleLogoContainer.frame.height/2
+        moduleName.text = moduleModel?.title
+        totalWorkout.text = "\(moduleModel?.exercise.count ?? 0)"
+        workoutTime.text = "\(moduleModel!.time):00"
     }
     
+    
+    
     @IBAction func onFinishButtonClicked(sender: UIButton) {
-        self.navigationController?.pushViewController(BreakExerciseViewController(), animated: true)
+        self.navigationController?.pushViewController(ModuleExerciseViewController(), animated: true)
     }
 
     /*
