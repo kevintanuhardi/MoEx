b1b9227915b82697e42f91dcd56c4b84b462b71d
Ahmad Zaky W <a.zaky.wisnumurti@mail.ugm.ac.id>
2021-06-19 20:29:34 +0700

    Change angle calculation

--- MoEx/Module/Doing Exercise/Custom Class/BodyPoseProcessor.swift
+++ MoEx/Module/Doing Exercise/Custom Class/BodyPoseProcessor.swift
@@ -9,13 +9,23 @@ import CoreGraphics
 
 class BodyPoseProcessor{
     
-    typealias AngleAxis = (firstAxis: CGPoint, secondAxis: CGPoint, center: CGPoint)
+    typealias AngleAxis = (firstAxis: CGPoint, secondAxis: CGPoint, thirdAxis: CGPoint)
     
+    //calculate the third axix / center
     static func calculateAngle(first: CGPoint, second: CGPoint, center: CGPoint) -> Double{
-        let numerator = Double((center.y * (first.x - second.x)) + (first.y * (second.x - center.x)) + (second.y * (center.x - first.x)))
-        let denominator = Double((center.x - first.x) * (first.x - second.x) + (center.y-first.y) * (first.y-second.y))
-        let ratio = numerator / denominator
-        let angleRad = atan(ratio)
+        let xDistA = center.x - second.x
+        let yDistA = center.y - second.y
+        let a = sqrt((xDistA * xDistA) + (yDistA * yDistA))
+        
+        let xDistB = first.x - second.x
+        let yDistB = first.y - second.y
+        let b = sqrt((xDistB * xDistB) + (yDistB * yDistB))
+        
+        let xDistC = center.x - first.x
+        let yDistC = center.y - first.y
+        let c = sqrt((xDistC * xDistC) + (yDistC * yDistC))
+        
+        let angleRad = acos(((a*a) + (c*c) - (b*b)) / ((2*a*c)))
         var angleDeg = (angleRad * 180)/Double.pi
         if angleDeg < 0 {
             angleDeg += 180
@@ -24,18 +34,21 @@ class BodyPoseProcessor{
         
     }
     
-    func processPoints(_ angleAxis: AngleAxis) {
+    func processPoints(_ angleAxis: AngleAxis) -> Double {
         let firstAxis = angleAxis.firstAxis
         let secondAxis = angleAxis.secondAxis
-        let center = angleAxis.center
-        print("angle",BodyPoseProcessor.calculateAngle(first: firstAxis, second: secondAxis, center: center))
+        let center = angleAxis.thirdAxis
+        
+        let angle = BodyPoseProcessor.calculateAngle(first: firstAxis, second: secondAxis, center: center)
+        print("angle", angle)
+        return angle
     }
 }
 
 // MARK: - CGPoint helpers
 
 extension CGPoint {
-
+    
     static func midPoint(p1: CGPoint, p2: CGPoint) -> CGPoint {
         return CGPoint(x: (p1.x + p2.x) / 2, y: (p1.y + p2.y) / 2)
     }
